53b3a19340417ddfa48e9a204ea7fe6f97677245
MichaelBelgium <michaelvrld@gmail.com>
2017-08-19 21:24:53 +0200

    Add vips table
    Might change in the future ofcourse

--- sql_dump.sql
+++ sql_dump.sql
@@ -1,3 +1,5 @@
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `Players`
 --
@@ -15,3 +17,26 @@ CREATE TABLE IF NOT EXISTS `Players` (
   `lIP` varchar(16) NOT NULL COMMENT 'Last IP',
   PRIMARY KEY (`Playername`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Vips`
+--
+
+CREATE TABLE IF NOT EXISTS `Vips` (
+  `Name` varchar(24) NOT NULL,
+  `Duration` int(11) NOT NULL DEFAULT '0',
+  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `Name` (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Vips`
+--
+ALTER TABLE `Vips`
+  ADD CONSTRAINT `Vips_ibfk_1` FOREIGN KEY (`Name`) REFERENCES `Players` (`Playername`) ON DELETE CASCADE ON UPDATE CASCADE;
